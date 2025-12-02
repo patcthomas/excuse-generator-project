1cc6babb938835601802564fa90e0ea0eb285b22
patcthomas <patcthomas28@gmail.com>
2025-12-02 01:10:57 +0000

    Rearranged JS page a little bit along with adding some extra prompts

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'My brother'];
+window.onload = function() {
+  //write your code here
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'My brother', 'My cat'];
 let action = ['ate', 'peed', 'crushed', 'broke', 'attacked'];
-let what = ['my homework', 'my phone', 'the car', 'my computer'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying', 'while I was gaming'];
+let what = ['my homework', 'my phone', 'the car', 'my computer', 'my pencil'];
+let when = ['before class', 'when I was sleeping', 'while I was jogging', 'during my lunch', 'while I was praying', 'while I was gaming', 'when I was in the bathroom'];
 
 function getRandomNumber(x, y) {
   return Math.floor(Math.random() * (y - x) + x);
@@ -20,9 +22,6 @@ function getRandom(anyArray){
   let random = getRandomNumber(min, max);
   return anyArray[random];
 }
-
-window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
   document.querySelector('#excuse').innerHTML = getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when)
 };
